2f0380641f35778f93fbd350e8a2362f6151716a
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-08-26 21:34:09 -0700

    add config struct

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,77 @@ use std::sync::mpsc;
 use rand::thread_rng;
 use tiny_keccak::Sha3;
 
+// Mining config parameters
+
+struct Config {
+    name: String,
+    params: String,
+    target: u32,
+    threads: u32
+}
+
+impl Config {
+    // Parse command line arguments
+
+    fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // Get function name
+
+        args.next();
+        let name = args.next()
+            .ok_or("no function name specified")?;
+        
+        // Get function parameters
+
+        let params = match args.next() {
+            Some(arg) => {
+                if !arg.starts_with("(") || !arg.ends_with(")") {
+                    return Err("expected function parameters in format (type1,type2,...)");
+                } else if arg.contains(char::is_whitespace) {
+                    return Err("function parameters must be in format (type1,type2,...) without spaces");
+                }
+                arg
+            },
+            None => return Err("no function parameters specified")
+        };
+
+        // Get zero byte target
+
+        let target = match args.next() {
+            Some(arg) => {
+                match arg.parse::<u32>() {
+                    Ok(value) => {
+                        if value > 4 {
+                            return Err("zero byte target must be less than or equal to 4");
+                        }
+                        value
+                    },
+                    Err(_) => return Err("could not parse zero byte target as integer")
+                }
+            },
+            None => return Err("no zero byte target specified")
+        };
+
+        // Get thread count
+
+        let threads = match args.next() {
+            Some(arg) => {
+                match arg.parse::<u32>() {
+                    Ok(value) => {
+                        if value == 0 {
+                            return Err("thread count must be larger than 0")
+                        }
+                        value
+                    },
+                    Err(_) => return Err("could not parse thread count as integer")
+                }
+            },
+            None => return Err("no thread count specified")
+        };
+
+        Ok(Config { name, params, target, threads })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
